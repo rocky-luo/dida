fca50c35aa5840a724ea6fd07ffec5d1d25d53cd
rocky <rocky.luo@ximalaya.com>
2018-02-22 15:46:10 +0800

    init

---
+++ dida-registry/src/main/java/com/rocky/dida/registry/Registry.java
@@ -0,0 +1,16 @@
+package com.rocky.dida.registry;
+
+import java.util.List;
+
+/**
+ * Created by rocky on 17/10/13.
+ */
+public interface Registry {
+    void register();
+    void unregister();
+
+    void subscribe(String url, NotifyListener listener);
+    void unSubscribe(String url, NotifyListener listener);
+
+    List<String> lookup(String url);
+}
